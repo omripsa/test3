84d73b710ae67a0cba1b6fcf7e26de94053b6be4
omripsa <omrish19@gmail.com>
2022-08-25 15:40:55 +0300

    commit

--- testHero/testHero/settings.py
+++ testHero/testHero/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vz=i&!yh3a+#3=x!gznj@=qks)08$uzz=r&m&bgcm1ilkj5(*2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','testname3.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1','testname3.herokuapp.com']
 
 
 # Application definition
